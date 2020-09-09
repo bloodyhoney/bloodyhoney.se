c98701338a1f89b9771f6827a4faf5c1697c29c6
Olle Burlin <olleburlin@Olles-MacBook-Pro.local>
2020-09-09 13:28:51 +0200

    Hero

--- src/components/layout-front.js
+++ src/components/layout-front.js
@@ -8,7 +8,7 @@ import Hero from "./index/Hero";
 function LayoutFront({ children }) {
   return (
     <div>
-      <div className="flex flex-col font-sans text-gray-900 header-hero">
+      <div className="flex flex-col font-sans text-gray-900 h-screen-80">
         <div>
           <Header />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   },
   theme: {
     extend: {
+      height: (theme) => ({
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+        "screen-80": "80vh",
+      }),
       colors: {
         grass: `#1a8642`,
       },
