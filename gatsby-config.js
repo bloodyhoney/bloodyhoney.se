c9c88fb6f5048b237c7ad0cff33c50514336b8fc
Olle Burlin <olleburlin@Olles-Mac-mini.local>
2021-04-07 11:48:51 +0200

    Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
                 ? // Lets just pull 50 posts in development to make it easy on ourselves.
                   50
                 : // and we don't actually need more than 5000 in production for this particular site
-                  50,
+                  5000,
           },
         },
       },
